7e5d9c30ec16717d9f05b543c8a489b8a78e7929
carlos <huaixian.huang@gmail.com>
2018-11-19 18:45:56 +0800

    Added: compatibility with different merge request url

--- contentScript.js
+++ contentScript.js
@@ -11,6 +11,7 @@ var gitlab_axios_instance = axios.create({
 window.addEventListener ("load", myMain, false);
 
 var mergeBtn;
+var acceptBtn;
 
 function myMain () {
   let removeSourceBranch = document.evaluate (
@@ -48,23 +49,26 @@ function myMain () {
 
     mergeBtnInstance.innerHTML = "";
     mergeBtnInstance.setAttribute("disabled", "disabled");
-    var newItem = document.createElement("Button");       // Create a <li> node
-    newItem.className = mergeBtnInstance.className;
+    acceptBtn = document.createElement("Button");       // Create a <li> node
+    acceptBtn.className = mergeBtnInstance.className;
     var textnode = document.createTextNode("Accept");  // Create a text node
-    newItem.addEventListener('click', function() {
+    acceptBtn.addEventListener('click', function() {
       console.log('comment button is click');
+      AcceptMR();
     });
-    newItem.appendChild(textnode);
-    // document.insertBefore(newItem, mergeBtnInstance.childNodes[0]);  // Insert <li> before the first child of <ul>
-    mergeSpanInstance.appendChild(newItem);
+    acceptBtn.appendChild(textnode);
+    // document.insertBefore(acceptBtn, mergeBtnInstance.childNodes[0]);  // Insert <li> before the first child of <ul>
+    mergeSpanInstance.appendChild(acceptBtn);
 
-    AcceptMR();
+    
   }
 }
 
 function GitlabParseURLInfo(url){
   let projectInfo = {};
-  [projectInfo.project,projectInfo.mr] =  lodash.split(lodash.split(url,"http://www.lejuhub.com/")[1],'/merge_requests/');
+  [projectInfo.groupname,projectInfo.projectname,projectInfo.type,projectInfo.mr] =  lodash.split(lodash.split(url,"http://www.lejuhub.com/")[1],'/');
+  console.log('projectInfo:',lodash.split(lodash.split(url,"http://www.lejuhub.com/")[1],'/'));
+  projectInfo.project = projectInfo.groupname + '/' + projectInfo.projectname;
   return projectInfo;
 }
 
@@ -95,7 +99,7 @@ function GitlabMrsCommits(mrs,history,callback){
     gitlab_axios_instance
       .get('/projects/'+ mr.project_id+'/merge_requests/'+ mr.iid +'/commits?per_page=100&page=1')
       .then((ret)=>{
-        history[history.length] = {id:mr.iid,commits:lodash.map(ret.data,(data)=>{
+        history[history.length] = {id:mr.iid,created_at:mr.created_at,commits:lodash.map(ret.data,(data)=>{
           return data.id;
         })};
         GitlabMrsCommits(lodash.slice(mrs,1),history,callback);})
@@ -109,45 +113,48 @@ function GitlabMrsCommits(mrs,history,callback){
 }
 
 function AcceptMR(){
+  acceptBtn.innerHTML = "Merging";
+  acceptBtn.setAttribute("disabled", "disabled");
   let curURL = document.URL;
   let urlInfo = GitlabParseURLInfo(curURL);
   console.log('urlinfo is;',urlInfo);
   QueryProjectMrs(1,100,urlInfo.project,[],(error,data)=>{
     console.log('error:',error,' data is:',data);
     if(!error){
-      //filter cur mr
-      let mrs = lodash.filter(data,(data)=>{
-        console.log(data.iid," ",urlInfo.mr);
-        // return (''+data.iid) !== urlInfo.mr;
-        return true;
-      });
-      console.log('mrs is:',mrs);
-      GitlabMrsCommits(mrs,[],(error,commits)=>{
+      GitlabMrsCommits(data,[],(error,commits)=>{
         console.log('error is:',error);
         if(!error){
-          console.log('commits is:',commits);
           let curCommits = lodash.filter(commits,(data)=>{
             return data.id == urlInfo.mr;
           })[0];
           let otherCommits = lodash.filter(commits,(data)=>{
             return data.id != urlInfo.mr;
           });
-          console.log('cur commits:',curCommits);
-          console.log('other commits:',otherCommits);
-          console.log('',lodash.filter(otherCommits,(commits)=>{
-            console.log('commits is:',commits," curcommits:",curCommits);
+          let crossMRs = lodash.filter(otherCommits,(commits)=>{
             let matched = false;
+            console.log('cur commits is:',curCommits);
             lodash.map(curCommits.commits,(commit)=>{
-              console.log('commits.commits:',commits.commits,' commit is:',commit);
               if(lodash.includes(commits.commits,commit)){
                 matched = true;
               }
             });
             return matched;
-          }));
+          });
+          let curCreatedAt = new Date(curCommits.created_at);
+          let foundOldCrossMr = false;
+          lodash.map(crossMRs,(mr)=>{
+            let mrCreatedAt = new Date(mr.created_at);
+            if(mrCreatedAt < curCreatedAt){
+              foundOldCrossMr = true;
+            }
+          });
+          console.log('Found old corss Mr:',foundOldCrossMr);
+          if(foundOldCrossMr){
+            acceptBtn.innerHTML = "Found crossed MR Can't Merge";
+            acceptBtn.setAttribute("disabled", "disabled");
+          }
         }
       });
-
     }
   });
 }
